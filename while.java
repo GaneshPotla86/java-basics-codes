eed1aef4328ed84589821a010c127437124296e4
GaneshPotla86 <potlaganesh563@gmail.com>
2025-09-05 20:17:05 +0530

    while loop code is added

---
+++ while.java
@@ -0,0 +1,18 @@
+//This code is used while loop syntax
+
+import java.util.*;
+ class Main {
+    public static void main(String[] args) {
+        int i=0;
+        while (i<5){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
+//OUTPUT: 0
+//1
+//2
+//3
+//4
