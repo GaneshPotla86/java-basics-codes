461d12b23a80c67c59eeaf203c622996daf4a932
GaneshPotla86 <potlaganesh563@gmail.com>
2025-09-05 20:20:32 +0530

    do while loop code is added

---
+++ dowhile.java
@@ -0,0 +1,14 @@
+// This code is for do while loop
+
+import java.util.*;
+ class Main {
+    public static void main(String[] args) {
+        int i=0;
+        do{       //Here in do while first it execute statement atleast once and then only it check the condition then.
+            System.out.println(i);
+        }while(i<0);
+    }
+}
+
+
+//OUTPUT: 0
