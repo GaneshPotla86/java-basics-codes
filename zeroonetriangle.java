6dd088a36e348ddafc02838beec49621378990ff
GaneshPotla86 <potlaganesh563@gmail.com>
2025-09-06 21:09:06 +0530

    0-1 triangle pattern code solution is added

---
+++ zeroonetriangle.java
@@ -0,0 +1,27 @@
+// This is the solution code for 0-1 Triangle
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n =5;
+        for(int i=1 ; i<=n;i++){
+            for(int j=1;j<=i;j++){
+                int sum =i+j;
+                if (sum % 2==0){
+                    System.out.print("1 ");
+                }else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+//OUTPUT:
+// 1
+// 0 1
+// 1 0 1
+// 0 1 0 1
+// 1 0 1 0 1
