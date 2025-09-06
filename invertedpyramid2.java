c65aedc5c6dd848435863b6c4a876162d9bde6ec
GaneshPotla86 <potlaganesh563@gmail.com>
2025-09-06 20:19:50 +0530

    Inverted Pyramid by 180 degrees is added

---
+++ invertedpyramid2.java
@@ -0,0 +1,18 @@
+//This is the pattern code for inverted pyramid rotated by 180 degrees
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n =4;
+        for(int i=1 ; i<=n;i++){
+            for (int j=1;j<=n-i;j++){
+                System.out.print("  ");
+            }
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
