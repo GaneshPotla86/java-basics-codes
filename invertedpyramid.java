33e0f763206df2ad20432ccb23c2abc6b46fb5d4
GaneshPotla86 <potlaganesh563@gmail.com>
2025-09-06 20:03:07 +0530

    Inverted Pyramid code is added

---
+++ invertedpyramid.java
@@ -0,0 +1,22 @@
+// This is the code for inverted pyramid pattern
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n =4;
+        for (int i =n;i>=1 ;i--){
+            for (int j=1 ;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+//OUTPUT:
+// * * * *
+// * * * 
+// * * 
+// * 
