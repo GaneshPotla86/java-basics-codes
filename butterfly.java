095b42f648c9df89dce6de40121bd362c7407176
GaneshPotla86 <potlaganesh563@gmail.com>
2025-09-07 19:30:14 +0530

    Butterfly pattern code is added

---
+++ butterfly.java
@@ -0,0 +1,55 @@
+// This th code for butterfly pattern
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        //upper-part
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            //spaces
+            int spaces= 2*(n-i);
+            
+            for(int j=1;j<=spaces;j++){
+                System.out.print("  ");
+            }
+            //second loop
+            for (int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        //2nd part
+        for (int i=n;i>=1;i--){
+            for (int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            int spaces= 2*(n-i);
+            
+            for(int j=1;j<=spaces;j++){
+                System.out.print("  ");
+            }
+            for (int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+        
+    }
+}
+
+
+//Output:
+//4
+//*             * 
+//* *         * * 
+//* * *     * * * 
+//* * * * * * * * 
+//* * * * * * * * 
+//* * *     * * * 
+//* *         * * 
+//*             * 
