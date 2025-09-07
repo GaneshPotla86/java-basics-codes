af5fb4edbce904e40fb4a7d608257eb6c7097c56
GaneshPotla86 <potlaganesh563@gmail.com>
2025-09-07 19:49:37 +0530

    Solid  Rhombus pattern code is pushed

---
+++ solidrhombus.java
@@ -0,0 +1,28 @@
+// This code is for Solid Rhombus Pattern printing
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for (int i=1;i<=n;i++){
+            int spaces= n-i;
+            for (int j=1;j<=spaces;j++){
+                System.out.print("  ");
+            }
+            for (int j=1;j<=n;j++){
+                System.out.print("* ");
+                
+            }
+            System.out.println();
+        }
+    }
+}
+
+//OUTPUT:
+
+//    *****
+//   *****
+//  *****  
+// *****
+//*****
